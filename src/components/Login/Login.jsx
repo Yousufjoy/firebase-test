311b77a9e6e91670ee9b280b7274c0f1b48ddd86
Yousufjoy <kanekiken333@gmail.com>
2023-09-29 23:24:11 +0600

    Open google login popup and Recap google popup sign

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,10 +3,18 @@ import app from "../../firebase/firebase.init";
 
 const Login = () => {
   const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
 
+  const provider = new GoogleAuthProvider();
+  // sigInWithPopup  dile she google er server e jabe then amake bolbe j thik moto paisi naki pai nai
   const handlGoogleSignIn = () => {
-    signInWithPopup(auth, provider).then(); // sigInWithPopup  dile she google er server e jabe then amake bolbe j thik moto paisi naki pai nai
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .then((error) => {
+        console.log("Error", error.message);
+      });
   };
   return (
     <div>
